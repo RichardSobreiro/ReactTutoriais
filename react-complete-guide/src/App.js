cb70caaae2a4dabff812450e1a8cd380f246e52c
Richard Luan Souza Sobreiro <richard.sobreiro@supermix.com.br>
2019-03-14 23:06:15 -0300

    rrrr

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -45,7 +45,8 @@ state = {
   
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       color: '1px solid blue',
       padding: '8px',
@@ -67,18 +68,30 @@ state = {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
     }
 
+    let classes = [];
+
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+
+
+
     return (
-      <div className="App">
-        <h1>React App</h1>
-        <p>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons
-        </button>
-        {persons}
-      </div>
+        <div className="App">
+          <h1>React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons
+          </button>
+          {persons}
+        </div>
     )
   }
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'React App'));
